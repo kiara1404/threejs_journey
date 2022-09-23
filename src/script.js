3166c3081970ad2e3355c7cf5e6b54ca42ec4559
Kiara <kiarabroekhuizen1@hotmail.com>
2022-09-23 15:01:46 +0200

    completed lesson 6

--- src/script.js
+++ src/script.js
@@ -1,5 +1,6 @@
 import './style.css';
 import * as THREE from 'three';
+import gsap from 'gsap';
 
 // Scene
 const scene = new THREE.Scene()
@@ -8,6 +9,7 @@ const scene = new THREE.Scene()
 const geometry = new THREE.BoxGeometry(1, 1, 1)
 const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
 const mesh = new THREE.Mesh(geometry, material)
+mesh.rotation.y = 0.5
 scene.add(mesh)
 
 // Sizes
@@ -26,4 +28,25 @@ const renderer = new THREE.WebGLRenderer({
     canvas: document.querySelector('canvas.webgl')
 })
 renderer.setSize(sizes.width, sizes.height)
-renderer.render(scene, camera)
+
+// Clock
+const clock = new THREE.Clock()
+
+gsap.to(mesh.position, {duration: 1, delay: 1, x: 2})
+gsap.to(mesh.position, {duration: 1, delay: 2, x: 0})
+// Animations
+const tick = () => {
+    // Clock
+    // const elapsedTime = clock.getElapsedTime()
+    
+    // // Update objects
+    // camera.position.y = Math.sin(elapsedTime)
+    // camera.position.x = Math.cos(elapsedTime)
+    // camera.lookAt(mesh.position)
+
+    // render object
+    renderer.render(scene, camera)
+
+    window.requestAnimationFrame(tick)
+}
+tick()
